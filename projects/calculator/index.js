cc584af10754728b00c7ad065b0e123551f766dc
Unco0k3d <ekim6755@gmail.com>
2020-10-26 13:37:48 -0400

    x(x) fixed

--- projects/calculator/index.js
+++ projects/calculator/index.js
@@ -3,10 +3,12 @@ const number = document.querySelector('.numbers')
 const remove = document.querySelector('.delete')
 const clear = document.querySelector('.clear')
 const inputScreen = document.querySelector('.input')
+const leftPerentheses = document.querySelector('.left-perentheses')
+const rightPerentheses = document.querySelector('.right-perentheses')
 const equals = document.querySelector('.equals')
-console.log(equals)
 
-const equation =[]
+const equation = []
+console.log(equation)
 
 number.addEventListener('click', numberInputOrRemove)
 operator.addEventListener('click', operatorInput)
@@ -15,32 +17,46 @@ function numberInputOrRemove(e){
     if(e.target.value!=='arrow'){
         const num = e.target.value
         equation.push(num)
-        console.log(equation)
-        console.log(inputScreen.value = equation.join(''))
+        // console.log(equation)
+        inputScreen.value = equation.join('')
     }
     if(e.target.value==='arrow'){
         equation.pop()
-        console.log(equation)
-        console.log(inputScreen.value=equation.join(''))
+        // console.log(equation)
+        inputScreen.value=equation.join('')
     }
 }
 function operatorInput(e){
     const op = e.target.value
     if(op===equals.value){
-        evaluate()
+        evaluate(equation)
     }
     else if(op===clear.value){
         equation.splice(0,equation.length)
+        // console.log(equation)
         inputScreen.value = equation
     }
     else{
         equation.push(op)
-        console.log(equation)
-        console.log(inputScreen.value = equation.join(''))
+        inputScreen.value = equation.join('')
     }
 }
-function evaluate(){
-    const total = eval(equation.join(''))
+function evaluate(equation){
+    equation.forEach(i=>{
+        const index = equation.indexOf(i)
+        const before = equation[index-1]
+        const after = equation[index+1]
+        if(i==='(')
+            if(before!==undefined)
+                if(before!=='+'&&before!=='/'&&before!=='-')
+                    equation.splice(index,1,['*','('])
+        if(i===')')
+            if(after!==undefined)
+                if(after!=='+'&&after!=='/'&&after!=='-')
+                    equation.splice(index,1,[')','*'])
+    })
+    console.log(equation)
+    const total = eval(equation.flat().join(''))
     inputScreen.value = total
     equation.splice(0,equation.length)
     equation.push(total)
